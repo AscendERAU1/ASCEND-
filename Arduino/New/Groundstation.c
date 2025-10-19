e90827ab0d801d60ab7ada0022acdb78869eb4c3
bostont3 <bostont3@my.erau.edu>
2025-10-19 13:33:54 -0700

    making c file correct in correct place

--- Arduino/New/Groundstation.c
+++ Arduino/New/Groundstation.c
@@ -12,9 +12,9 @@ float ForwardAzimuth = 0;
 float ElevationAngle = 0;
 float PI = 3.14159265358979323846;
 float maxIter=200;
-float tol=1*e^(-12);
+float tol=1*exp(-12);
 
-void azmuth_Range() {
+void azmuth_Range(double coordinate1[2], double coordinate2[2], float altitude1, float altitude2) {
 
   //In void defines
   float phi_1, phi_2, L_1, U_1, U_2, L, L_2, L_1, lamda, sin_lamda, cos_lamda, cos_sigma, sin_sigma, sigma;
@@ -33,7 +33,7 @@ void azmuth_Range() {
 
   U_1 = atan((1 - self.f) * tan(phi_1));
   U_2 = atan((1 - self.f) * tan(phi_2));
- L = L_2 - L_1  // Change in longitude
+ L = L_2 - L_1;  // Change in longitude
 lamda = L;
 // Initialize values used in iterations
 sin_U1 = sin(U_1);
@@ -42,26 +42,36 @@ sin_U2 = sin(U_2);
 cos_U2 = cos(U_2); // Cannot initialize lamda because it iterates
 
 for (int i=0;i<maxIter;i++){
-sin_lamda = sin(lamda)
-cos_lamda = cos(lamda)  # Initialize lamda angles within iteration
-sin_sigma = sqrt((cos_U2 * sin_lamda) ^ 2 + (cos_U1 * sin_U2 - sin_U1 * cos_U2 * cos_lamda) ^ 2)
-cos_sigma = sin_U1 * sin_U2 + cos_U1 * cos_U2 * cos_lamda
-sigma = atan2(sin_sigma, cos_sigma)
-sin_alpha = (cos_U1 * cos_U2 * sin_lamda) / sin_sigma
-cos2_alpha = 1 - sin_alpha ** 2
-cos2sigmam = cos_sigma - (2 * sin_U1 * sin_U2 / cos2_alpha)
-C = (self.f / 16) * cos2_alpha * (4 + self.f * (4 - 3 * cos2_alpha))
-lamda_prev = lamda
-lamda = L + (1 - C) * self.f * sin_alpha * (
-sigma + C * sin_sigma * (cos2sigmam + C * cos_sigma * (-1 + 2 * cos2sigmam ** 2)))
-test = abs(lamda_prev - lamda)
-}
+sin_lamda = sin(lamda);
+cos_lamda = cos(lamda);  // Initialize lamda angles within iteration
+sin_sigma = sqrt((cos_U2 * sin_lamda) ^ 2 + (cos_U1 * sin_U2 - sin_U1 * cos_U2 * cos_lamda) ^ 2);
+cos_sigma = sin_U1 * sin_U2 + cos_U1 * cos_U2 * cos_lamda;
+sigma = atan2(sin_sigma, cos_sigma);
+sin_alpha = (cos_U1 * cos_U2 * sin_lamda) / sin_sigma;
+cos2_alpha = 1 - sin_alpha ^ 2;
+cos2sigmam = cos_sigma - (2 * sin_U1 * sin_U2 / cos2_alpha);
+C = (self.f / 16) * cos2_alpha * (4 + self.f * (4 - 3 * cos2_alpha));
+lamda_prev = lamda;
+lamda = L + (1 - C) * self.f * sin_alpha * (sigma + C * sin_sigma * (cos2sigmam + C * cos_sigma * (-1 + 2 * cos2sigmam ^ 2)));
+test = abs(lamda_prev - lamda);
+
+    if (test <= tol){
+    break;
+    }
+
+    }
+    return 0;
 }
 
-void setup() {
-}
+void main(){
 
-void loop() {
-  // put your main code here, to run repeatedly:
+    double coordinate1[2] = {34.0522, -118.2437};  // Example: Los Angeles
+    double coordinate2[2] = {40.7128, -74.0060};   // Example: New York
+    float altitude1 = 100.0;  // meters
+    float altitude2 = 500.0;  // meters
+
+    azmuth_Range(coordinate1, coordinate2, altitude1, altitude2);
+
+    return 0;
 }
 
