fae86d6fa7fc38360db9ed9b37e8277c30c4da21
Alex Nie <alex.yf.nie@gmail.com>
2025-11-07 17:19:52 -0700

    updated testing for mini ground station

--- GroundStationMini/src/main.cpp
+++ GroundStationMini/src/main.cpp
@@ -1,37 +1,42 @@
 #include <Arduino.h>
 #include <AccelStepper.h>
 
-#define CLOCKWISE 0
-#define COUNTER_CLOCKWISE 1
+#define NUM_PINS 4
+#define STEPS_PER_REV 100
 
 using namespace std;
 
-const int IN1 = 11;
-const int IN2 = 10;
-const int IN3 = 9;
-const int IN4 = 8;
-
-// const int BLUE = IN1;
-// const int RED = IN2;
-// const int GREEN = IN3;
-// const int BLACK = IN4;
-
-AccelStepper stepper1(4, IN1, IN2, IN3, IN4);
+AccelStepper stepper1(NUM_PINS, 11, 10, 9, 8);
+AccelStepper stepper2(NUM_PINS, 7, 6, 5, 4);
 void testBackAndForth();
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   stepper1.setMaxSpeed(1000);
-  stepper1.setAcceleration(500);
+  stepper1.setAcceleration(10000);
+  stepper1.setMinPulseWidth(1);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  testBackAndForth();
+  for(int i = 0; i < 10; i++){
+    for(int j = 0; j < 30000; j++ ){
+      stepper1.setSpeed(pow(2, i));
+      stepper1.run();
+    }
+  }
+  for(int i = 10; i > 0; i--){
+    for(int j = 0; j < 30000; j++ ){
+      stepper1.setSpeed(pow(2, i)); 
+      stepper1.run();
+    }
+  }
+  Serial.println("Cycled");
+  // testBackAndForth();
 }
 
-int position = 200;
+int position = 1000;
 
 void testBackAndForth(){
   Serial.println(String(stepper1.currentPosition()));
@@ -40,7 +45,7 @@ void testBackAndForth(){
   if(stepper1.distanceToGo() == 0){
     Serial.println("Reached Target");
     stepper1.disableOutputs();
-    delay(5000); // Wait for 2 seconds
+    delay(10000); // Wait for 2 seconds
     position = -position; // Reverse target position
   }else{
     stepper1.enableOutputs();
