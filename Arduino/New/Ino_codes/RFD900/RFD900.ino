a19c6bbda60499b2382d4575797c9159a10fe9a8
bostont3 <bostont3@my.erau.edu>
2026-02-04 18:52:36 -0700

    Added a different rfd900 code
    NOT CURRENTLY COMMENTTED OUT
    they are stacked on eachother so they will run against eachother and possibly not compile

--- Arduino/New/Ino_codes/RFD900/RFD900.ino
+++ Arduino/New/Ino_codes/RFD900/RFD900.ino
@@ -1,7 +1,9 @@
+// this was the original
+
 #define LIGHT 12
 
 void setup() {
-  Serial.begin(9600); 
+  Serial.begin(57600); 
   pinMode(LIGHT, OUTPUT);
 
   delay(2000);
@@ -14,4 +16,24 @@ void loop() {
   digitalWrite(LIGHT, LOW);
 
   delay(1000);
+}
+
+// this is a different one that could be used
+#define LIGHT 12
+#include <SoftwareSerial.h>
+
+SoftwareSerial rfd(-1, 9); // RX disabled, TX only
+
+void setup() {
+  pinMode(LIGHT, OUTPUT);
+  delay(2000);
+  Serial.println("RFD900 transmitter online");
+  rfd.begin(57600); // must match radio config
+}
+
+void loop() {
+  rfd.println("Hello from Arduino");
+  digitalWrite(LIGHT, HIGH);
+  digitalWrite(LIGHT, LOW);
+  delay(2000);
 }
